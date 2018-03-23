6e5990d80dad438c9923891a5b87e002e121d53e
soap51 <soap51@outlook.com>
2018-03-23 07:51:15 +0700

    user sign up

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import path from 'path';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import auth from './routes/auth';
+import users from './routes/users';
 import dotenv from 'dotenv';
 import Promise from 'bluebird';
 dotenv.config();
 const app =express();
 app.use(bodyParser.json())
+mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URL);
 app.use('/api/auth' , auth);
+app.use('/api/users' , users);
 app.get('/*' , (req,res) =>{
     res.sendFile(path.join(__dirname, 'index.html'));
 })
